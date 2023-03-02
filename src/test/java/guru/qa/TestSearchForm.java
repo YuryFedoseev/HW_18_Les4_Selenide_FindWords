6080a47fe18382c12031b277da4208e90e961ff0
Yury.Fedoseev <Yri95@inbox.ru>
2023-03-02 19:19:16 +0300

    readme.md

--- src/test/java/guru/qa/TestSearchForm.java
+++ src/test/java/guru/qa/TestSearchForm.java
@@ -1,2 +1,34 @@
-package guru.qa;public class TestSearchForm {
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestSearchForm {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1220x880";
+        Configuration.holdBrowserOpen = true;
+    }
+
+
+    @Test
+    void searchText() {
+        open("https://github.com/");
+
+        $("[placeholder= 'Search GitHub']").setValue("Selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
+        $(".markdown-body").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $(".Layout-main").shouldHave(text("Using JUnit5 extend")).scrollIntoView(false);
+        // screenshot("scrin.jpg");
+
+    }
+
 }
+
